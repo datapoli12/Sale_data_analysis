83e7c0d32311ce32734ceef04b757a6ce0389fe6
Apollinaire Monteiro <apollinairemonteiro@gmail.com>
2024-06-06 14:17:23 +0200

    deuxième version actualisée, création du fichier .ipynb en .py

--- sale_data.py
+++ sale_data.py
@@ -3,3 +3,10 @@ import pandas as pd
 path = "C:\\Users\\monte\\OneDrive\\Desktop\\Sale_data_analysis\\sale_data.csv"
 sdf = pd.read_csv(path)
 print(sdf)
+
+# information sur la database
+
+print(type(sdf))
+print(len(sdf.columns))
+print(len(sdf.index))
+sdf.info()
